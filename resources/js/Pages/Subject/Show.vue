ffc6281de53784bb26d678b40b10290e0fdb6361
Ryan <vellabonelloryan@gmail.com>
2023-04-30 08:15:04 +0800

    adds stats to header section of Subject/Show

--- resources/js/Pages/Subject/Show.vue
+++ resources/js/Pages/Subject/Show.vue
@@ -9,7 +9,23 @@ const props = defineProps({
 <template>
   <AppLayout :selectedSubject="subject.slug">
     <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ subject.title }}</h2>
+      <div class="flex justify-between">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ subject.title }}</h2>
+        <ul class="flex gap-x-8">
+          <li>
+            <i class="fa-solid fa-chart-line-up mr-2"></i>
+            <span>2300xp</span>
+          </li>
+          <li>
+            <i class="fa-solid fa-chalkboard-user mr-2"></i>
+            <span>14 lessons</span>
+          </li>
+          <li>
+            <i class="fa-solid fa-ballot-check mr-2"></i>
+            <span>55 quizzes</span>
+          </li>
+        </ul>
+      </div>
     </template>
 
     <div class="p-8">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Icons -->
+        <script src="https://kit.fontawesome.com/dcce82336f.js" crossorigin="anonymous"></script>
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
