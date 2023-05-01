8c7902cd9a1d6d3c878d496805d9ef32a7d1b1df
Ryan <vellabonelloryan@gmail.com>
2023-05-01 12:31:33 +0800

    adds `deleted_at` column to Lesson

--- database/migrations/2023_05_01_035836_create_lessons_table.php
+++ database/migrations/2023_05_01_035836_create_lessons_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreignId('tutor_id')->constrained()->onDelete('CASCADE');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
